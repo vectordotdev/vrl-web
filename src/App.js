a4639a4061ee9e70a34dc32592cdd9034451e637
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-13 14:05:54 -0700

    Get initial Tailwind setup working
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import data from "./scenarios.yaml";
 import axios from "axios";
 import { createGlobalState } from 'react-hooks-global-state';
+import './style.css';
 
-var n = 0;
-const initialState = { scenario: data[n], output: null, resolved: null };
+const initialState = { scenario: data[0], output: null, resolved: null };
 const { useGlobalState } = createGlobalState(initialState);
 const vrlWebServerAddress = process.env.VRL_WEB_SERVER_ADDRESS;
 const resolveEndpoint = `${vrlWebServerAddress}/resolve`;
@@ -21,45 +21,51 @@ function Navbar() {
     setOutput(null);
   }
 
-  return <nav>
-    <span>
-      The VRL Playground
-    </span>
+  return <nav className="bg-gray-200 py-2 px-4">
+    <div className="flex justify-between items-center">
+      <span className="text-3xl font-bold">
+        The VRL Playground
+      </span>
 
-    <ul>
-      {data.map(s => (
-        <li key={s.id}>
-          <button onClick={() => updateScenario(s.id)}>
-            {s.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+      <ul className="flex space-x-2">
+        {data.map(s => (
+          <li key={s.id} className="hover:text-gray-700">
+            <button onClick={() => updateScenario(s.id)}>
+              {s.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   </nav>
 }
 
 function Event(props) {
   return <div>
-    <p>
+    <p className="text-lg font-semibold">
       Event
     </p>
 
-    <span>
-      {JSON.stringify(props.event)}
-    </span>
+    <Code code={props.event} />
   </div>
 }
 
 function Program(props) {
   return <div>
-    <p>
+    <p className="text-lg font-semibold">
       Program
     </p>
 
-    <span>
-      {JSON.stringify(props)}
-    </span>
+    <Code code={props.program} />
   </div>
+}
+
+function Code(props) {
+  const formatted = (typeof props.code === 'object') ? JSON.stringify(props.code, null, 2) : props.code;
+
+  return <pre className="font-mono bg-black text-white p-3 text-sm rounded">
+    {formatted}
+  </pre>
 }
 
 function Main(props) {
@@ -81,47 +87,51 @@ function Main(props) {
       });
   }
 
-  return <div>
-    <Event event={props.scenario.event} />
+  return <div className="p-4">
+    <div className="grid grid-cols-2 gap-x-8">
+      <Event event={props.scenario.event} />
 
-    <Program program={props.scenario.program} />
+      <Program program={props.scenario.program} />
+    </div>
 
-    {resolved != null && (
-      <div>
-        <p>
-          Resolved
-        </p>
-
-        <span>
-          {JSON.stringify(resolved)}
-        </span>
-
-        {output != null && (
+    <div className="mt-4 grid grid-cols-2 gap-x-8">
+      {resolved != null && (
+        <>
           <div>
-            <p>
-              Output
+            <p className="text-lg font-semibold">
+              Resolved
             </p>
 
-            <span>
-              {JSON.stringify(output)}
-            </span>
+            <Code code={resolved} />
           </div>
-        )}
-      </div>
-    )}
 
-    <button onClick={resolve}>
-      Resolve
-    </button>
+          {output != null && (
+            <div>
+              <p className="text-lg font-semibold">
+                Output
+              </p>
+
+              <Code code={output} />
+            </div>
+          )}
+        </>
+      )}
+    </div>
+
+    <div className="mt-6">
+      <button onClick={resolve} className="border py-1.5 px-2 rounded-md bg-gray-200">
+        Resolve
+      </button>
+    </div>
   </div>
 }
 
 export function App() {
   const [scenario] = useGlobalState('scenario');
 
-  return <>
+  return <div className="font-sans antialiased bg-gray-50 min-h-screen flex flex-col">
     <Navbar />
 
     <Main scenario={scenario} />
-  </>
+  </div>
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.js', './src/**/*.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
