6ccdfe9a767f456c5a1a0bc45648a590467bbb0d
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-13 12:07:18 -0700

    Add some example scenarios via YAML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import data from "./scenarios.yaml";
+
 export function App() {
-  return <div>
-    <p>
-      Hello world
-    </p>
-  </div>;
+  const scenarios = data;
+
+  return <>
+    <nav>
+      <h1>
+        The VRL Playground
+      </h1>
+    </nav> 
+
+    <main>
+      <h2>
+        Scenarios
+      </h2>
+
+      <span>
+        {scenarios.map(s => (
+          <div>
+            <h3>
+              {s.title}
+            </h3>
+
+            <p>
+              Event
+            </p>
+
+            <span>
+              {JSON.stringify(s.event)}
+            </span>
+
+            <p>
+              Program
+            </p>
+
+            <span>
+              {s.program}
+            </span>
+          </div>
+        ))}
+      </span>
+    </main>
+  </>;
 }
