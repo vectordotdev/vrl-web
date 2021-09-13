62b23d419d18f4e82e0ce1204a1f048e6253556c
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-13 12:30:33 -0700

    Add initial global state mechanism
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import data from "./scenarios.yaml";
+import { createGlobalState } from 'react-hooks-global-state';
+
+var n = 0;
+const initialState = { scenario: data[n] };
+const { useGlobalState } = createGlobalState(initialState);
 
 export function App() {
-  const scenarios = data;
+  const [scenario, setScenario] = useGlobalState('scenario');
+
+  const changeScenario = () => {
+    n = (n == 0) ? 1 : 0;
+    setScenario(data[n]);
+  }
 
   return <>
-    <nav>
-      <h1>
-        The VRL Playground
-      </h1>
-    </nav> 
+    <h1>
+      The VRL Playground
+    </h1>
 
-    <main>
-      <h2>
-        Scenarios
-      </h2>
+    <p>
+      {JSON.stringify(scenario)}
+    </p>
 
-      <span>
-        {scenarios.map(s => (
-          <div>
-            <h3>
-              {s.title}
-            </h3>
-
-            <p>
-              Event
-            </p>
-
-            <span>
-              {JSON.stringify(s.event)}
-            </span>
-
-            <p>
-              Program
-            </p>
-
-            <span>
-              {s.program}
-            </span>
-          </div>
-        ))}
-      </span>
-    </main>
-  </>;
+    <button onClick={changeScenario}>
+      Change it up
+    </button>
+  </>
 }
