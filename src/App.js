a91ce2e5e6e23319eb97d048e55b5e6a1688c64e
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-13 13:26:57 -0700

    Add resolution mechanism
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import data from "./scenarios.yaml";
+import axios from "axios";
 import { createGlobalState } from 'react-hooks-global-state';
 
 var n = 0;
-const initialState = { scenario: data[n] };
+const initialState = { scenario: data[n], output: null, resolved: null };
 const { useGlobalState } = createGlobalState(initialState);
 
 function Navbar() {
+  const [resolved, setResolved] = useGlobalState('resolved');
+  const [output, setOutput] = useGlobalState('output');
   const [scenario, setScenario] = useGlobalState('scenario');
 
   function updateScenario(id) {
     const scenario = data.filter(s => s.id === id)[0];
     setScenario(scenario);
+    setResolved(null);
+    setOutput(null);
   }
 
   return <nav>
@@ -49,16 +54,62 @@ function Program(props) {
     </p>
 
     <span>
-      {props.program}
+      {JSON.stringify(props)}
     </span>
   </div>
 }
 
 function Main(props) {
+  const [resolved, setResolved] = useGlobalState('resolved');
+  const [output, setOutput] = useGlobalState('output');
+
+  function resolve() {
+    const payload = {
+      event: props.scenario.event,
+      program: props.scenario.program
+    }
+
+    axios.post('http://localhost:8080/resolve', payload)
+      .then(res => {
+        console.log(res);
+
+        setOutput(res.data.success.output);
+        setResolved(res.data.success.result);
+      });
+  }
+
   return <div>
     <Event event={props.scenario.event} />
 
     <Program program={props.scenario.program} />
+
+    {resolved != null && (
+      <div>
+        <p>
+          Resolved
+        </p>
+
+        <span>
+          {JSON.stringify(resolved)}
+        </span>
+
+        {output != null && (
+          <div>
+            <p>
+              Output
+            </p>
+
+            <span>
+              {JSON.stringify(output)}
+            </span>
+          </div>
+        )}
+      </div>
+    )}
+
+    <button onClick={resolve}>
+      Resolve
+    </button>
   </div>
 }
 
