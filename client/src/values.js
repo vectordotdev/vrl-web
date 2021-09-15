edc9af389a7c4667662c2d5224c0874b219af242
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-15 15:46:08 -0700

    Fix editor options export issue
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- client/src/values.js
+++ client/src/values.js
@@ -6,7 +6,7 @@ export const VRL_RESOLVE_ENDPOINT = `${VRL_WEB_SERVER_ADDRESS}/resolve`;
 export const VRL_FUNCTIONS_ENDPOINT = `${VRL_WEB_SERVER_ADDRESS}/functions`;
 export const SCENARIOS = data;
 
-const EDITOR_OPTIONS = {
+export const EDITOR_OPTIONS = {
   lineNumbers: false,
   padding: {
     bottom: 10,
