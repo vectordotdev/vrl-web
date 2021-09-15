62d38886f4aec3739768087b3403184eadd116dc
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-14 19:38:55 -0700

    Remove unused components
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import { vrlWebServerAddress } from "../values";
-
-export function Footer() {
-  return <footer className="bg-gray-200 py-3 px-4">
-    <p>
-      Connected to the server at <strong>{vrlWebServerAddress}</strong>.
-    </p>
-  </footer>
-}
