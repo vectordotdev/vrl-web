0a35c29650ea5cf426198cf5f811a3a1cbfe8253
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-15 19:12:39 -0700

    Remove Tailwind build warning about extension blocks
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.js', './src/**/*.html'],
   darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  theme: {},
+  variants: {},
   plugins: [],
 }
