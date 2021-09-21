a28908c86c47e9e2459ca878b964e1804d3b0a45
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-20 20:17:27 -0700

    Add server addresses via environment variable
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- client/src/values.js
+++ client/src/values.js
@@ -32,3 +32,4 @@ export const HOST = process.env.HOST;
 export const VRL_WEB_SERVER_ADDRESS = process.env.VRL_WEB_SERVER_ADDRESS;
 export const VRL_RESOLVE_ENDPOINT = `${VRL_WEB_SERVER_ADDRESS}/resolve`;
 export const VRL_FUNCTIONS_ENDPOINT = `${VRL_WEB_SERVER_ADDRESS}/functions`;
+export const VRL_INFO_SERVER_ADDRESS = process.env.VRL_INFO_SERVER_ADDRESS;
