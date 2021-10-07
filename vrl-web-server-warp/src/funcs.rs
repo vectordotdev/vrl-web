af928c71da01fdbcccb623d35160c7f3926f2bcd
Luc Perkins <luc.perkins@datadoghq.com>
2021-10-07 10:45:41 -0700

    Add more function info to /functions endpoint
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- vrl-web-server-warp/src/funcs.rs
+++ vrl-web-server-warp/src/funcs.rs
@@ -1,15 +1,52 @@
 use serde::Serialize;
 use std::convert::Infallible;
+use vrl::prelude::Kind;
 use warp::{reply::json, Reply};
 
 #[derive(Serialize)]
-struct FunctionList {
-    functions: Vec<&'static str>,
+struct Example {
+    title: &'static str,
+    source: &'static str,
+}
+
+#[derive(Serialize)]
+struct Parameter {
+    name: &'static str,
+    kind: &'static str,
+    required: bool,
+}
+
+#[derive(Serialize)]
+struct Function {
+    name: &'static str,
+    parameters: Vec<Parameter>,
+    examples: Vec<Example>,
 }
 
 pub async fn function_metadata() -> Result<impl Reply, Infallible> {
-    let functions: Vec<&'static str> = vrl_stdlib::all().iter().map(|f| f.identifier()).collect();
-    let functions_list = FunctionList { functions };
+    let functions: Vec<Function> = vrl_stdlib::all()
+        .iter()
+        .map(|f| Function {
+            name: f.identifier(),
+            parameters: f
+                .parameters()
+                .iter()
+                .map(|p| Parameter {
+                    name: p.keyword,
+                    kind: Kind::new(p.kind).as_str(),
+                    required: p.required,
+                })
+                .collect(),
+            examples: f
+                .examples()
+                .iter()
+                .map(|e| Example {
+                    title: e.title,
+                    source: e.source,
+                })
+                .collect(),
+        })
+        .collect();
 
-    Ok(json(&functions_list))
+    Ok(json(&functions))
 }
