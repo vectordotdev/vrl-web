5f0e67fa7ae79502add3d3bef81410436d985dc7
Luc Perkins <luc.perkins@datadoghq.com>
2021-10-08 09:51:12 -0700

    Remove hotkeys dependency
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- vrl-web-client/src/components/App.tsx
+++ vrl-web-client/src/components/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useHotkeys } from "react-hotkeys-hook";
 
 import { vrlInfo } from "../data/vrl";
 
@@ -9,9 +8,6 @@ import { Router } from "./Router";
 
 export const App = () => {
   const setVrlInfo: () => void = vrlInfo.store(s => s.setVrlInfo);
-
-  const [count, setCount] = useState(0);
-  useHotkeys('ctrl+k', () => setCount(prevCount => prevCount + 1));
 
   // Initial page load logic
   useEffect(() => {
