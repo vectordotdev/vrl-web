7b7ff7c4a2e5b1cefddfeca10b7bb0d16811ebc9
Luc Perkins <luc.perkins@datadoghq.com>
2021-10-14 15:18:59 -0700

    Provide basic page layout
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- vrl-web-client/components/Layout.tsx
+++ vrl-web-client/components/Layout.tsx
@@ -1,11 +1,45 @@
-import { Box } from "@chakra-ui/layout";
+import { Alert } from "@chakra-ui/alert";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import React from "react";
 
 type Props = {
   children: JSX.Element;
 }
 
+
 export default function Layout({ children }: Props) {
-  return <Box p={4}>
-    {children}
-  </Box>
+  return <Flex
+    minH="100vh"
+    direction="column"
+  >
+    <Box
+      p={4}
+      bgColor="black"
+    >
+      <Text
+        color="white"
+      >
+        Navbar
+      </Text>
+    </Box>
+
+    <Box
+      p={4}
+      flex="1"
+      bgColor="gray.50"
+    >
+      {children}
+    </Box>
+
+    <Box
+      p={4}
+      bgColor="black"
+    >
+      <Text
+        color="white"
+      >
+        Footer
+      </Text>
+    </Box>
+  </Flex>
 }
--- vrl-web-client/pages/index.tsx
+++ vrl-web-client/pages/index.tsx
@@ -3,12 +3,8 @@ import { NextPage } from "next";
 
 const Index: NextPage = () => {
   return <>
-    <h1>
-      Welcome home!
-    </h1>
-
     <p>
-      Learn more about <Link href="/vrl">VRL</Link>.
+      Learn more about <Link href="/vrl">VRL</Link> here.
     </p>
   </>
 }
