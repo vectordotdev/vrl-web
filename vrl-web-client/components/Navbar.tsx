fba0e07260ab8ae33d9fc923d7e22f1b6eebf2d1
Luc Perkins <luc.perkins@datadoghq.com>
2021-10-20 19:14:38 -0700

    Add color mode toggle
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- vrl-web-client/components/Navbar.tsx
+++ vrl-web-client/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@chakra-ui/button";
+import { Button, IconButton } from "@chakra-ui/button";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import VectorDarkIcon from "./icons/VectorDarkIcon";
 import VectorLightIcon from "./icons/VectorLightIcon";
@@ -7,20 +8,29 @@ import VectorLightIcon from "./icons/VectorLightIcon";
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  const icon = (colorMode == "light") ? <VectorLightIcon /> : <VectorDarkIcon />;
+  const isDark = colorMode == "dark";
+  const vectorIcon = isDark ? <VectorDarkIcon /> : <VectorLightIcon />;
+  const colorModeIcon = isDark ?
+    <SunIcon boxSize={5} color="yellow.500" /> :
+    <MoonIcon boxSize={5} color="blue.600" />;
 
   const bg = useColorModeValue("gray.100", "black");
 
   return <Flex
-    py={4}
-    px={6}
+    py={5}
+    px={8}
     justifyContent="space-between"
+    alignItems="center"
     bgColor={bg}
   >
-    {icon}
+    {vectorIcon}
 
-    <Button onClick={toggleColorMode}>
-      Click me
-    </Button>
+    <IconButton
+      size="md"
+      aria-label="Color mode toggler"
+      variant="ghost"
+      icon={colorModeIcon}
+      onClick={toggleColorMode}
+    />
   </Flex>
 }
