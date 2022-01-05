e6ec1fb4e6495f73cc83e45c45a76cfb7c998466
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-05 21:17:41 +0800

    Delete vrl-web-client directory

--- vrl-web-client/src/ui/mode.ts
+++
@@ -1,7 +0,0 @@
-import { DarkModeConfig } from "use-dark-mode";
-
-export const darkModeUserPreference: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-export const darkModeConfig: DarkModeConfig = {
-  classNameDark: "dark",
-};
